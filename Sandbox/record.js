f3b0d7fac701423c522dffb953fe8db6a6d1c14f
finteger <104397474+finteger@users.noreply.github.com>
2023-08-15 08:47:42 -0600

    Add files via upload

---
+++ Sandbox/record.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+
+const recordSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true},
+    attendanceCount: {type: Number, default: 0},
+    absentCount: {type: Number, default: 0},
+});
+
+
+module.exports = mongoose.model('Record', recordSchema);
